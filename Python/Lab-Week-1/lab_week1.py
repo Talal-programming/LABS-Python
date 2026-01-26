abbed42a909d2685362668531bc36c15a38fdd2c
Talal-programming <talaljop123@gmail.com>
2026-01-26 16:35:38 +0300

    Create lab_week1.py

---
+++ Python/Lab-Week-1/lab_week1.py
@@ -0,0 +1,32 @@
+# Lab Week 1
+# Student Name: Talal
+# Course: Python Programming
+# Description: Basic Python syntax and data types.
+
+
+print("Welcome to Python Programming!")
+
+
+print("Name: Talal")
+print("Course: Python Programming")
+
+
+
+student_name = "Alice"                  # String
+age = 20                                # Integer
+gpa = 3.75                             # Float
+subjects = ["Math", "Python", "Physics"]  # List
+student_info = {"id": 101, "year": 2}   # Dictionary
+skills = {"Python", "SQL"}              # Set
+is_enrolled = True                     # Boolean
+graduation_date = None                 # NoneType
+
+# Print variables and their data types
+print(student_name, type(student_name))
+print(age, type(age))
+print(gpa, type(gpa))
+print(subjects, type(subjects))
+print(student_info, type(student_info))
+print(skills, type(skills))
+print(is_enrolled, type(is_enrolled))
+print(graduation_date, type(graduation_date))
